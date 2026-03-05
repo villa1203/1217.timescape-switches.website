2b3efa71229048df9cf2b2b911f73ba8b8bd29ed
azertypow <nico@villa1203.ch>
2026-03-05 15:26:28 +0100

    eat: footer height

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -3,51 +3,56 @@
     >
       <div style="width: 100%; height: 1px; background: white; margin-bottom: var(--app-gutter)"/>
 
-      <div class="app-grid app-grid--justify-between">
-        <div class="v-app-footer__left-content">
-          <div class="app-text-h1">
-            We are always happy to meet new people. Give us a call, email or come visit us.
+      <div class="v-app-footer__container app-grid app-grid--justify-between app-grid--direction-column">
+
+        <div class="app-grid app-grid--justify-between">
+          <div class="v-app-footer__left-content">
+            <div class="app-text-h1">
+              We are always happy to meet new people. Give us a call, email or come visit us.
+            </div>
+          </div>
+
+          <div>
+            <img class="v-app-footer__logo--anim"
+                 src="/logo__anime.gif"
+            />
           </div>
         </div>
 
-        <div>
-          <img class="v-app-footer__logo--anim"
-               src="/logo__anime.gif"
-          />
-        </div>
-      </div>
+        <div class="app-grid app-grid--justify-between">
+          <div>
+            <img class="v-app-footer__logo"
+                 src="/logo.svg" />
+          </div>
 
-      <div class="app-grid app-grid--justify-between">
-        <div>
-          <img class="v-app-footer__logo"
-               src="/logo.svg" />
-        </div>
+          <div>
+            <div>Rue de Lyon 22</div>
+            <div>Genève 1203</div>
+            <div>Switzerland</div>
+          </div>
 
-        <div>
-          <div>Rue de Lyon 22</div>
-          <div>Genève 1203</div>
-          <div>Switzerland</div>
-        </div>
+          <div>
+            <div>24 Rue Royale</div>
+            <div>Lyon 69001</div>
+            <div>France</div>
+          </div>
 
-        <div>
-          <div>24 Rue Royale</div>
-          <div>Lyon 69001</div>
-          <div>France</div>
-        </div>
+          <div>
+            <div>Contact</div>
+            <div>hello@bureau1217.com</div>
+            <div>+41 705 80 87 90</div>
+          </div>
 
-        <div>
-          <div>Contact</div>
-          <div>hello@bureau1217.com</div>
-          <div>+41 705 80 87 90</div>
-        </div>
-
-        <div>
-          <div class="app-grid app-grid--justify-end app-grid--align-center">
-            <button class="app-button app-button--variant-primary">Linkedin</button>
-            <button class="app-button app-button--variant-primary">Instagram</button>
+          <div>
+            <div class="app-grid app-grid--justify-end app-grid--align-center">
+              <button class="app-button app-button--variant-primary">Linkedin</button>
+              <button class="app-button app-button--variant-primary">Instagram</button>
+            </div>
           </div>
         </div>
+
       </div>
+
     </footer>
 </template>
 
@@ -74,9 +79,12 @@ defineProps<{
   width: 100%;
 }
 
+.v-app-footer__container {
+  height: calc(100vh - 7rem);
+}
+
 .v-app-footer__left-content {
   max-width: 40em;
-  margin-bottom: 20rem;
 }
 
 .v-app-footer__logo {
