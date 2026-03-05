f032b88a88c9b9993bbe78e0df6e631263570e08
azertypow <nico@villa1203.ch>
2026-03-05 17:44:49 +0100

    feat: add infos

--- app/components/AppNav.vue
+++ app/components/AppNav.vue
@@ -3,11 +3,42 @@
     >
       <div class="app-grid app-grid--justify-between">
 
-        <nuxt-link to="/" class="app-button">
+        <div class="app-button" style="z-index: 10"
+             :class="{'infos-is-open': infosIsOpen}"
+        >
           <div class="app-grid app-grid--align-center">
-            <img src="/logo.svg" /><UIOpen/>
+            <nuxt-link to="/">
+              <img src="/logo.svg" role="button"/>
+            </nuxt-link>
+            <div @click="infosIsOpen = !infosIsOpen">
+              <div class="toggle-infos">
+                <UIOpen/>
+              </div>
+            </div>
           </div>
-        </nuxt-link>
+        </div>
+
+        <div v-if="infosIsOpen"
+             class="app-rm-child-margin v-nav__infos"
+        >
+          <p>
+            <span class="app-text-strong">informations</span>
+            <br>Daisy Chain is a new motion studio co-directed by Harry Butt and Connor Campbell with a focus on playful, experimental, vibrant aesthetics.</p>
+          <p>We work with 3D, 2D and generative animation to create music videos, brand identities and typography in motion.</p>
+
+          <p>
+            <span class="app-text-strong">Get in touch</span>
+            <br>295 Hoxton Street
+            <br>London
+            <br>N1 5JX
+          </p>
+
+          <div style="margin-top: 10rem; padding-bottom: 1rem;">
+            <span class="app-button app-button--negative">Instagram</span>
+            <span class="app-button app-button--negative">LinkedIn</span>
+          </div>
+
+        </div>
 
         <nav class="app-grid app-grid--justify-end"
              style="gap: .5rem"
@@ -26,12 +57,12 @@
 
 
 <script setup lang="ts">
-import { defineProps } from 'vue'
 import UIOpen from "~/components/UIOpen.vue";
 
-const props = defineProps<{
-    message?: string
-}>()
+const infosIsOpen = ref(false)
+
+
+
 </script>
 
 
@@ -41,5 +72,28 @@ const props = defineProps<{
 <style lang="scss" scoped >
 .v-nav {
   width: 100%;
+  position: relative;
+}
+
+.v-nav__infos {
+  position: absolute;
+  top: var(--app-gutter);
+  left: var(--app-gutter);
+  width: 30rem;
+  background: hsla(0, 0%, 0%, 0.25);
+  border-radius: .75rem;
+  color: white;
+  padding: 5rem .75rem .75rem;
+  z-index: 0;
+}
+
+.infos-is-open {
+  background: transparent;
+  transition: none;
+  backdrop-filter: none;
+
+  .toggle-infos {
+    transform: rotate(45deg);
+  }
 }
 </style>
