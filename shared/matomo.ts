9f6e7220d4f505bd3b7e1a5d586dfdeff1653f2e
azertypow <nico@villa1203.ch>
2026-06-11 11:24:18 +0200

    chore: set matomo site ID and url

--- shared/matomo.ts
+++ shared/matomo.ts
@@ -14,9 +14,9 @@ export function initMatomo() {
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
-      var u="//matomo.notre-historia.ch/";
+      var u="//matomo.timescape-switches.ch/";
       _paq.push(['setTrackerUrl', u+'matomo.php']);
-      _paq.push(['setSiteId', '2']);
+      _paq.push(['setSiteId', '1']);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
       g.async=true; g.src=u+'matomo.js'; s!.parentNode!.insertBefore(g,s!);
     })();
