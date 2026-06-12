dc1b7941aa2fa4e599d23f542db99024cee3c579
azertypow <nico@villa1203.ch>
2026-06-12 20:36:29 +0200

    chore: unify text link color

--- app/components/BlockBulletPoint.vue
+++ app/components/BlockBulletPoint.vue
@@ -80,11 +80,6 @@ defineProps<{
   margin: 0;
 }
 
-/* CMS text links: brand violet instead of the browser default blue. */
-.block-bullet-point__item__text :deep(a) {
-  color: var(--app-color-primary);
-  text-decoration: underline;
-}
 
 .block-bullet-point__item__text :deep(a:hover) {
   text-decoration: none;
