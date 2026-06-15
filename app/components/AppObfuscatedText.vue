67e47807bb383a1716f88822c3ef929a5a4f2731
azertypow <nico@villa1203.ch>
2026-06-15 15:11:31 +0200

    feat: ne obfuscated text decoding

---
+++ app/components/AppObfuscatedText.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="v-app-obfuscated-text"
+         data-nosnippet
+         v-html="decodedText"
+    />
+</template>
+
+
+
+
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import {decodeObfuscatedHTML} from "~/utils/decodeObfuscatedHTML";
+
+const props = defineProps<{
+    text: string
+}>()
+
+const decodedText = ref(props.text)
+
+onMounted(() => {
+  const tempText = props.text.replaceAll(':::decode', '').replaceAll(':::', '')
+  decodedText.value = tempText
+  decodeObfuscatedHTML(props.text, decodedText)
+})
+
+
+</script>
+
+
+
+
+
+<style lang="scss" scoped >
+.v-app-obfuscated-text {
+}
+</style>
---
+++ app/utils/decodeObfuscatedHTML.ts
@@ -0,0 +1,26 @@
+export function decodeObfuscatedHTML(
+  obfuscatedText: string,
+  propsToUpdate: Ref<string>,
+) {
+
+  let mouseMoved = false
+
+  function decodeEncodedBlocks(text: string) {
+    return text.replaceAll(/:::decode\s([\s\S]*?):::/g, (fullMatch, encodedText: string) => {
+      return `${atob(encodedText)}`
+    })
+  }
+
+  function onUserGesture() {
+    if (mouseMoved) return
+
+    mouseMoved = true
+    window.setTimeout(() => {
+      propsToUpdate.value = decodeEncodedBlocks(obfuscatedText)
+    }, 0)
+  }
+
+  window.addEventListener('mousemove', onUserGesture)
+  window.addEventListener('touchstart', onUserGesture)
+}
+
